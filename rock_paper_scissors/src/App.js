9b32e85cb72d15e7871ba4bc0b1d9c27e43599b9
Blair <blairacarroll@gmail.com>
2020-04-02 01:48:58 -0400

    react hooks added for wins, losses, and draws

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [draws, setDraws] = useState(0);
+
   return (
     <div className="main">
-
+      <p>So far you've won {wins} times!</p>
+      <button onClick={() => setWins(wins + 1)}>Rock</button>
+      <button>Paper</button>
+      <button>Scissors</button>
     </div>
   );
 }
