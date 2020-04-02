96e0b04a9a0e07cd87ec1da8cd60c2a7ef0e641e
Blair <blairacarroll@gmail.com>
2020-04-02 02:23:03 -0400

    letsPlay function created for running the game

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  
+  const letsPlay = (e) => {
+    setPlayerChoice(e.target.value)
+  }
+
   const [wins, setWins] = useState(0);
   const [losses, setLosses] = useState(0);
   const [draws, setDraws] = useState(0);
+  const [games, setGames] = useState(0);
 
   const [playerChoice, setPlayerChoice] = useState('');
   const [computerChoice, setComputerChoice] = useState('');
 
   return (
     <div className="main">
-      <p>So far you have {wins} WINS, {losses} LOSSES, and {draws} DRAWS</p>
+      <p>So far you have {wins} WINS, {losses} LOSSES, and {draws} DRAWS out of {games}</p>
       <p>You picked {playerChoice} </p>
-      <button onClick={() => setPlayerChoice('Rock')}>Rock</button>
-      <button onClick={() => setPlayerChoice('Paper')}>Paper</button>
-      <button onClick={() => setPlayerChoice('Scissors')}>Scissors</button>
+      <button name="Rock" value="Rock" onClick={letsPlay}>Rock</button>
+      <button name="Paper" value="Paper" onClick={letsPlay}>Paper</button>
+      <button name="Scissors" value="Scissors" onClick={letsPlay}>Scissors</button>
     </div>
   );
 }
