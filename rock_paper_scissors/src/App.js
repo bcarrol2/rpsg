d46bb6c4a6b0590ffe778eb56e4273000e67ccee
Blair <blairacarroll@gmail.com>
2020-04-02 02:07:55 -0400

    react hooks setup for player and computer choices

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -6,12 +6,16 @@ function App() {
   const [losses, setLosses] = useState(0);
   const [draws, setDraws] = useState(0);
 
+  const [playerChoice, setPlayerChoice] = useState('');
+  const [computerChoice, setComputerChoice] = useState('');
+
   return (
     <div className="main">
-      <p>So far you've won {wins} times!</p>
-      <button onClick={() => setWins(wins + 1)}>Rock</button>
-      <button>Paper</button>
-      <button>Scissors</button>
+      <p>So far you have {wins} WINS, {losses} LOSSES, and {draws} DRAWS</p>
+      <p>You picked {playerChoice} </p>
+      <button onClick={() => setPlayerChoice('Rock')}>Rock</button>
+      <button onClick={() => setPlayerChoice('Paper')}>Paper</button>
+      <button onClick={() => setPlayerChoice('Scissors')}>Scissors</button>
     </div>
   );
 }
