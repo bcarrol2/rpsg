06729ebc9691295e97de99515b45ee33e3c2952b
Blair <blairacarroll@gmail.com>
2020-04-02 02:35:42 -0400

    lodash installed and used for sample

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
 import './App.css';
 
 
 function App() {
   
-  const letsPlay = (e) => {
-    setPlayerChoice(e.target.value)
+  const letsPlay = (event) => {
+    setPlayerChoice(event.target.value)
+    setComputerChoice(randomPick)
   }
 
   const [wins, setWins] = useState(0);
   const [losses, setLosses] = useState(0);
   const [draws, setDraws] = useState(0);
   const [games, setGames] = useState(0);
+  const computerOptions = ['Rock', 'Paper', 'Scissors']
+  let randomPick = _.sample(computerOptions)
 
   const [playerChoice, setPlayerChoice] = useState('');
   const [computerChoice, setComputerChoice] = useState('');
@@ -19,7 +23,7 @@ function App() {
   return (
     <div className="main">
       <p>So far you have {wins} WINS, {losses} LOSSES, and {draws} DRAWS out of {games}</p>
-      <p>You picked {playerChoice} </p>
+      <p>You picked {playerChoice} and the computer picked {computerChoice} </p>
       <button name="Rock" value="Rock" onClick={letsPlay}>Rock</button>
       <button name="Paper" value="Paper" onClick={letsPlay}>Paper</button>
       <button name="Scissors" value="Scissors" onClick={letsPlay}>Scissors</button>
