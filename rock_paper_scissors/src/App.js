bac3a09d1d808313701a8ac9a90422b8ef84eef0
Blair <blairacarroll@gmail.com>
2020-04-03 12:37:21 -0400

    wild and styled -- with cheat code added

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import './App.css';
 
@@ -6,7 +6,7 @@ function App() {
 
   const letsPlay = (event) => {
     setDisable(true)
-    setTimeout(() => { setDisable(false) }, 1000);
+    setTimeout(() => { setDisable(false) }, 2000);
     let playerPick = event.target.value
     setPlayerChoice(playerPick)
     setComputerChoice(randomPick)
@@ -26,6 +26,8 @@ function App() {
       setLosses(losses + 1)
     } else if (playerPick === 'Scissors' && randomPick === 'Paper') {
       setWins(wins + 1)
+    } else if (playerPick === 'Gun') {
+      setWins(wins + 1)
     } else {
       alert('Wait, what now?');
     }
@@ -44,15 +46,38 @@ function App() {
 
   const [disable, setDisable] = useState(false);
 
+  const cheatPress = useKeyPress('g');
+
   return (
     <div className="overlay">
-      <p>So far you have {wins} WINS, {losses} LOSSES, and {draws} DRAWS out of {games} games</p>
+      <p>So far you have <span style={{color: 'green'}}>{wins}</span> WINS, <span style={{color: 'red'}}>{losses}</span> LOSSES, and <span style={{color: 'black'}}>{draws}</span> DRAWS out of <span>{games}</span> games</p>
       <p>You picked {playerChoice} and the computer picked {computerChoice} </p>
       <button disabled={disable} value="Rock" onClick={letsPlay}>Rock</button>
       <button disabled={disable} value="Paper" onClick={letsPlay}>Paper</button>
       <button disabled={disable} value="Scissors" onClick={letsPlay}>Scissors</button>
+      {cheatPress && <button id="gun-button" disabled={disable} value="Gun" onClick={letsPlay}>Gun</button>}
     </div>
   );
+}
+
+function useKeyPress(targetKey) {
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  // If pressed key is our target key then set to true
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return keyPressed;
 }
 
 export default App;
